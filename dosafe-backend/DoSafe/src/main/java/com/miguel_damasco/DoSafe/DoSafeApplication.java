f0e25d7e4c0b7099a685f6fe79f4cc35b093d690
Miguel Damasco <migueldamascogh@gmail.com>
2026-01-27 15:52:51 -0300

    Spring proyect Created

---
+++ dosafe-backend/DoSafe/src/main/java/com/miguel_damasco/DoSafe/DoSafeApplication.java
@@ -0,0 +1,13 @@
+package com.miguel_damasco.DoSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoSafeApplication.class, args);
+	}
+
+}
