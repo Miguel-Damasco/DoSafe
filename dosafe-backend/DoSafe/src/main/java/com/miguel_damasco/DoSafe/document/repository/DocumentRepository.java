eb6e6fc3279fbbbff6300b067e0fbfbe0593d0ee
Miguel Damasco <migueldamascogh@gmail.com>
2026-02-03 16:22:33 -0300

    DocumentRepository compilation error fixed

--- dosafe-backend/DoSafe/src/main/java/com/miguel_damasco/DoSafe/document/repository/DocumentRepository.java
+++ dosafe-backend/DoSafe/src/main/java/com/miguel_damasco/DoSafe/document/repository/DocumentRepository.java
@@ -1,9 +1,11 @@
 package com.miguel_damasco.DoSafe.document.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.miguel_damasco.DoSafe.document.domain.DocumentModel;
 
 @Repository
-public interface DocumentRepository extends JpaRepository<DocumentModel, String> {}
+public interface DocumentRepository extends JpaRepository<DocumentModel, UUID> {}
